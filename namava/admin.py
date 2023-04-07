413f583f691a1370043a63d41898d0585b2567c9
Ryan <sailantht@gmail.com>
2023-04-07 10:40:51 +0330

    Movie model

--- namava/admin.py
+++ namava/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Genre, Movie
 
-# Register your models here.
+admin.site.register(Genre)
+admin.site.register(Movie)
--- namava/models.py
+++ namava/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+    summary = models.CharField(max_length=500)
+    release_year = models.SmallIntegerField(
+        validators=[MinValueValidator(1300), MaxValueValidator(1500)]
+    )
+    rate = models.SmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+    duration = models.SmallIntegerField()
+    genre = models.ManyToManyField(Genre)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.title
